7552ce5559cecc9dd12c54f7e7c7f8b4652132f5
AhmadHassan1102 <ahmad.alihussain289@gmail.com>
2022-01-19 08:11:39 +0500

    nn

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 //dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
 
 module.exports = {
-    mongoURI: `mongodb://localhost:27017/baburao`
+    //mongoURI: `mongodb://localhost:27017/baburao`
+    mongoURI: `mongodb+srv://husnain:nawaz@cluster.0jmcl.mongodb.net/baburao`
 };
+    
--- config/passport.js
+++ config/passport.js
@@ -15,6 +15,7 @@ module.exports = function(passport) {
           return done(null, false, { message: 'That email is not registered' });
         }
         //Match password
+        console.log(role);
         if(role.password==password)
         {
           return done(null, role);
